4eb654e5ae44d796cc24cf7c78a02d29f25dbf66
strobie <0xstrobe@protonmail.com>
2024-01-01 19:51:45 +0000

    add arbitrum

--- src/types.ts
+++ src/types.ts
@@ -5,9 +5,9 @@ export type IfEquals<T, U, Y=true, N=false> =
   (<G>() => G extends T ? 1 : 2) extends
   (<G>() => G extends U ? 1 : 2) ? Y : N;
 
-export type SupportedChain = "bsc" | "ethereum";
+export type SupportedChain = "bsc" | "ethereum" | "arbitrum";
 export const isSupportedChain = (chain: string): chain is SupportedChain => {
-  return ["bsc", "ethereum"].includes(chain);
+  return ["bsc", "ethereum", "arbitrum"].includes(chain);
 };
 
 export interface AdapterHandler<T, U> {
